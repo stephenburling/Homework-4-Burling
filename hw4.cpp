644faa83688da1a3cb054a413f8cded17bee04ef
stephenburling <s_b471@txstate.edu>
2020-09-28 19:08:01 -0500

    changing insert

--- hw4.cpp
+++ hw4.cpp
@@ -145,6 +145,7 @@ list_t insert_list(list_t first_list, list_t second_list, unsigned int n) {
     cout << "\nfirst_list: " << endl;
 
     list_print(first_list);
+
     cout << "\nsecond_list: " << endl;
     list_print(second_list);
 
@@ -155,7 +156,16 @@ list_t insert_list(list_t first_list, list_t second_list, unsigned int n) {
     int size_first = list_size(first_list, 0);
 
     int size_second = list_size(second_list, 0);
-    
+    if (list_isEmpty(first_list)) {
+
+        return second_list;
+
+    }
+    else if (list_isEmpty(second_list)) {
+
+        return first_list;
+
+    }
 
     if (n >= size_first) {
 
